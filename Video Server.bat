407f85931cc39df6164a064c763eb1a010b09b0b
sinuoc <45698324+sinuoc@users.noreply.github.com>
2020-04-23 22:46:37 -0700

    Delete Video Server.bat

--- Video Server.bat
+++
@@ -1,4 +0,0 @@
-echo off
-cd "c:\ffmpeg\bin"
-ffmpeg -y -rtbufsize 1024M -hwaccel dxva2 -f dshow -i video="HD Webcam C615" -vcodec libx264 -f hls -hls_time 2.0 -hls_list_size 0 -hls_wrap 15 C:\inetpub\wwwroot\Webcam\Buffer\Cam01\Cam01.m3u8
-
